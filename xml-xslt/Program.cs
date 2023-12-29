49fe9278e5bc2d8f1a7c7b3348c421b09655c4b1
Selaka Nanayakkara <selaka.kithmal@gmail.com>
2023-12-29 17:55:25 +0530

    initial RnD

--- xml-xslt/Program.cs
+++ xml-xslt/Program.cs
@@ -9,16 +9,16 @@ class Program
         try
         {
             // Input XML file and XSLT file paths
-            string inputXmlFilePath = "path/to/your/input.xml";
-            string xsltFilePath = "path/to/your/remove_namespaces.xslt"; // Replace with the actual path
+            string inputXmlFilePath = "G:\\RND\\XML-to-XSLT transformer\\xml-xslt\\Commissioning_001000785839_361223_20231213054950750.biztemp";
+            string xsltFilePath = "G:\\RND\\XML-to-XSLT transformer\\xml-xslt\\CommissionFile.xml"; // Replace with the actual path
 
             // Output file path
-            string outputXmlFilePath = "path/to/your/output.xml"; // Replace with the desired output path
+            string outputXmlFilePath = "G:\\RND\\XML-to-XSLT transformer\\xml-xslt\\outputs\\output.xml"; // Replace with the desired output path
 
             // Load the XSLT transform
             XslCompiledTransform xslt = new XslCompiledTransform();
             xslt.Load(xsltFilePath);
-
+            XmlDocument original = new XmlDocument();
             // Create an XmlReader for the input XML file
             using (XmlReader reader = XmlReader.Create(inputXmlFilePath))
             {
@@ -36,6 +36,9 @@ class Program
 
                     Console.WriteLine($"Transformation complete. Output written to: {outputXmlFilePath}");
                 }
+                original.LoadXml(File.ReadAllText(outputXmlFilePath));
+                var originalXmlFileNodes = original.DocumentElement?.SelectNodes("//*")?.Cast<XmlNode>()?.Select(e => e.LocalName)?.Distinct()?.ToList();
+
             }
         }
         catch (Exception ex)
